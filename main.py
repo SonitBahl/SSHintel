2602b9e73e000ddf509e2194166c44c8b0c3b560
Sonit Bahl <sonitbahl@gmail.com>
2025-06-24 16:01:20 +0530

    added logger and handler for credentials

--- main.py
+++ main.py
@@ -4,8 +4,12 @@ from logging.handlers import RotatingFileHandler
 logging_format = logging.Formatter('%(message)s')
 funnel_logger = logging.getLogger('FunnelLogger')
 funnel_logger.setLevel(logging.INFO)
-
 funnel_handler = RotatingFileHandler('audit.log', maxBytes = 2000, backupCount = 5)
 funnel_handler.setFormatter(logging_format)
-
 funnel_logger.addHandler(funnel_handler)
+
+creds_logger = logging.getLogger('CredsLogger')
+creds_logger.setLevel(logging.INFO)
+creds_handler = RotatingFileHandler('cmd_audit.log', maxBytes=2000, backupCount=5)
+creds_handler.setFormatter(logging_format)
+creds_logger.addHandler(creds_handler)
